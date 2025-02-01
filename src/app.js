1b5cb67a1936d6f4ab9c6367b5febd4bf313046a
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-02-01 23:19:39 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,16 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
 app.use(
     cors({
-        origin: [
-            "https://viewcell.onrender.com/",
-        ],
-        credentials: true,
+      origin: "https://viewcell.onrender.com", 
+      credentials: true, 
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
     })
-);
-
+  );
+  
+  
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 app.use(express.static("public"));
