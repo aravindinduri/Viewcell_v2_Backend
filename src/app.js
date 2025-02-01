324188488252ccc7c5dc90730781bcb05eb3e8f3
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-02-01 18:16:34 +0530

    added

--- src/app.js
+++ src/app.js
@@ -4,16 +4,8 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-    cors({
-        origin: ["https://viewcell.onrender.com", "http://localhost:5173"], 
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
-    })
-);
+app.use(cors())
 
-app.options("*", cors());
 
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
