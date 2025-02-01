9be669396b8facad784f108d669c2840cbffb8b3
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-02-01 17:03:33 +0530

    added changes

--- src/app.js
+++ src/app.js
@@ -10,9 +10,12 @@ app.use(
             "https://viewcell.onrender.com",
             "http://localhost:5173",
         ],
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     })
-);
+)
+
 
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
