e8f9faf9f7c5b2a423ab49387195ba3056532e8b
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-02-02 22:55:57 +0530

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -7,11 +7,9 @@ app.use(
     cors({
       origin: "https://viewcell.onrender.com", 
       credentials: true, 
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
     })
   );
-  
+
   
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
